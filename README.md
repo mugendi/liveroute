7215f841b444bdcef5b35354f1f2a9faaa39c45a
Anthony Mugendi <ngurumugz@gmail.com>
2023-08-23 20:06:46 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+<!--
+ Copyright (c) 2023 Anthony Mugendi
+
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
+# liveRoute
+Watch and reload your routes whenever they change without using tools like [nodemon](https://www.npmjs.com/package/nodemon).
+
+## How to use
+
+Assume you have the router file **./routes/files.js**
+
+```javascript
+const express = require('express');
+const router = express.Router();
+
+// directory browser route 
+router.get('/:dir', async (req, res, next) => {
+    let { dir } = req.params;
+
+    // .... do whatever
+
+    res.json({ dir, files:[]});
+});
+
+module.exports = router;
+```
+
+This is how you would 'live-load' it...
+
+```javascript
+const express = require('express');
+const app = express();
+
+// require
+const LiveRoute = require('liveroute');
+// initialize by passing app
+const liveRoute = new LiveRoute(app);
+
+// then, pass the route & router file
+liveRoute.use('/files', './routes/files.js');
+```
+
+Now, behind the scenes, **liveRoute** will load the route file using: 
+`app.use('/files', require('./routes/files.js'))`
+
+Then, **liveRoute** uses [chokidar](https://www.npmjs.com/package/chokidar) to watch for any changes and reloads the file on change.
+
+It is of course a little more complex than explained here but ion general, I use [this approach](https://codeburst.io/dont-use-nodemon-there-are-better-ways-fc016b50b45e) approach.
---
+++ index.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2023 Anthony Mugendi
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+
+const path = require('path');
+const fs = require('fs');
+const chokidar = require('chokidar');
+const reload = require('require-reload')(require);
+
+class HotRouter {
+    constructor(app) {
+        if (!app.hasOwnProperty('listen') || typeof app.listen !== 'function') {
+            throw new Error(
+                'You can only instanciate HotRouter with a valid Express App Instance. Got ',
+                typeof app
+            );
+        }
+
+        this.routers = {};
+
+        this.app = app;
+    }
+
+    use(routePath = '*', routerFile = null) {
+        let self = this;
+
+        if (
+            typeof routePath !== 'string' ||
+            // simple route validation
+            /^[\*\/]/.test(routePath) == false
+        ) {
+            throw new Error('2nd should be a route/path such as "/api"');
+        }
+
+        const routerPath = path.resolve(routerFile);
+        if (!fs.existsSync(routerPath)) {
+            throw new Error(`Route File "${routePath} does not exist!`);
+        }
+
+        const router = reload(routerPath);
+
+        // simple test to ensure is router
+        if (
+            !router ||
+            'stack' in router === false ||
+            !Array.isArray(router.stack)
+        ) {
+            throw new Error('1st argument should be a router object.');
+        }
+
+        // ok we can use this router
+        self.routers[routerPath] = router;
+
+        // watch & reload file on change
+        // this ensures we only require the file when it has changed & not with every call
+        chokidar.watch(routerPath).on('change', (filePath) => {
+            console.log(routerPath, ' changed...');
+            self.routers[routerPath] = reload(routerPath);
+        });
+
+        this.app.use(routePath, function (req, res, next) {
+            return self.routers[routerPath](req, res, next);
+        });
+    }
+}
+
+module.exports = HotRouter;
